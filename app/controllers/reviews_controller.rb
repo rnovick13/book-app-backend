95c06b6bd3c6f0f8a465efbc2e36f4e98c551c3c
rnovick13 <rnovick13@gmail.com>
2019-09-24 17:49:08 -0400

    review create built and routes

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,6 +5,13 @@ class ReviewsController < ApplicationController
   end
 
   def create
-
+    @review = Review.create(review_params)
+    render json: @review
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:name, :body)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :books, only: [:index, :show]
   resources :authors, only: [:index, :show]
-  resources :reviews, only: [:index, :show]
+  resources :reviews, only: [:index, :show, :create, :update, :delete]
 end
