b3943c6c10dedb714aaca27fca68a9cfca264b8c
rnovick13 <rnovick13@gmail.com>
2019-11-09 10:39:54 -0500

    changed favorite route and added create method and params

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,22 @@ class FavoritesController < ApplicationController
     render json: @favorites
   end
 
+  def create
+    @favorite = Favorite.create(review_params)
+    render json: @favorite
+  end
+
   def show
 
   end
+
+  def delete
+    
+  end
+
+  private
+    def review_params
+      params.require(:favorite).permit(:book_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show]
   resources :authors, only: [:index, :show]
   resources :reviews, only: [:index, :show, :create, :update, :delete]
-  resources :favorites, only: [:index, :show, :create, :update, :delete]
+  resources :favorites, only: [:index, :show, :create, :delete]
 end
