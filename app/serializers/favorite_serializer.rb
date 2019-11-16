ac124725ae8923848188d8c12db69ba04c5d4d8d
rnovick13 <rnovick13@gmail.com>
2019-11-16 02:15:52 -0500

    favorite serializer

--- app/serializers/favorite_serializer.rb
+++ app/serializers/favorite_serializer.rb
@@ -2,6 +2,8 @@ class FavoriteSerializer < ActiveModel::Serializer
   attributes :id
   def book
     {book_id: self.object.book.id,
-    title: self.object.title}
+    title: self.object.title,
+    author: self.object.author,
+    cover: self.object.cover}
   end
 end
